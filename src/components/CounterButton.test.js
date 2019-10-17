5d8efa489968b35e068a31b3a11a23918da987e4
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-17 17:34:49 +0100

    adding a test for component props

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -16,5 +16,6 @@ it('correctly increments the couner', () => {
     expect(wrapper.state()).toEqual({count: 1});
     wrapper.find('[id="counter"]').simulate('click');
     expect(wrapper.state()).toEqual({count: 2});
+    expect(wrapper.props().color).toEqual("red");
 
 })
